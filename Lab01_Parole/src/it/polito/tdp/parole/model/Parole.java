8dcfccc18bd228c1d305dcbfb14c1f79a5fd6164
Alberto Monge <alberto@MacBook-Pro-di-Alberto.local>
2018-03-05 15:30:51 +0100

    Java project

---
+++ Lab01_Parole/src/it/polito/tdp/parole/model/Parole.java
@@ -0,0 +1,23 @@
+package it.polito.tdp.parole.model;
+
+import java.util.List;
+
+public class Parole {
+		
+	public Parole() {
+		//TODO
+	}
+	
+	public void addParola(String p) {
+		//TODO
+	}
+	
+	public List<String> getElenco() {
+		//TODO
+		return null;
+	}
+	
+	public void reset() {
+	}
+
+}
